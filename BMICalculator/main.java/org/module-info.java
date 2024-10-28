860cb6dace6e3fa2366d3e6609c4f1e90ae22610
Adakhan <147408734+Adahan73@users.noreply.github.com>
2024-10-28 16:32:48 +0600

    Update module-info.java

--- BMICalculator/main.java/org/module-info.java
+++ BMICalculator/main.java/org/module-info.java
@@ -1,11 +1,11 @@
-module BMICalculatorApp {
-    // Requires the javafx base, controls, and fxml modules
+module org.example.democalc {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
-    // Opens the package containing your FXML and controller to javafx.fxml
-    opens your.package.name to javafx.fxml;
 
-    // Exports the package containing your application main class
-    exports your.package.name;
+    opens org.example.democalc to javafx.fxml;
+    exports org.example.democalc;
+    exports BMICalculatorApp;
+    opens BMICalculatorApp to javafx.fxml;
 }
