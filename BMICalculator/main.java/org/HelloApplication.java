a701686d95ef194baff3e02920e1268522bbcb19
Adakhan <147408734+Adahan73@users.noreply.github.com>
2024-10-28 16:32:11 +0600

    Update HelloApplication.java

--- BMICalculator/main.java/org/HelloApplication.java
+++ BMICalculator/main.java/org/HelloApplication.java
@@ -1,27 +1,23 @@
-package BMICalculatorApp;
+package org.example.democalc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class HelloApplication extends Application {
-
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("BMICalculator.fxml")));
-        Scene scene = new Scene(root, 300, 200);
-        primaryStage.setTitle("BMI Calculator");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
-
